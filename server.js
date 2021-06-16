b07971da86b3f3153a9aa17657e8e906fc5c0691
luongchung <chunglv42@wru.vn>
2021-06-17 01:19:56 +0700

    chung edit

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 var express =require("express");
 var app =express();
-app.use(express.static("public")); //chỉ rằng public là thư mục root
+app.use(express.static("public")); 
 app.set("view engine","ejs");
 app.set("views","./views")
 var server =require("http").Server(app);
-var io =require("socket.io")(server);
-server.listen(1997);//process.env.PORT
-var arrUser=[];
-io.on("connection",function(socket){
-  console.log("Có người kết nối !");  //Lắng nghe có người kết nối
-});
+
+const fs = require('fs');
+
+
+//let data = JSON.stringify(student);
+//fs.writeFileSync('default.json', data);
+
+
+server.listen(2000);
 app.get("/",function (req,res) {
     res.render("trangchu");
 })
+
+app.get("/config",function (req,res) {
+  let rawdata = fs.readFileSync('default.json');
+  var result = removeDefaultJSON(JSON.parse(req.query.data),JSON.parse(rawdata));
+  res.send(result);
+})
+
+function removeDefaultJSON(src,defaultJson){
+  console.log(src);
+  for (var attrname in src) {
+    if(defaultJson.hasOwnProperty(attrname)) {
+      if (src[attrname] == defaultJson[attrname]) {
+        console.log("delete => "+ attrname);
+        delete src[attrname];
+      } 
+    }
+  }
+  return src;
+}
